62f8451760934299f8cc80261cdfcf68eecd7cc7
daegwan00 <daegwanou@gmail.com>
2024-06-10 01:47:30 +0900

    custom_appbar만듬 이유: 컴포넌트들이 너무 나눠져있음 하나로 통합

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<Header></Header>*/}
-    {/*<App></App>*/}
-    <SignUp></SignUp>
+    <App></App>
+    {/*<SignUp></SignUp>*/}
   </React.StrictMode>
 );
 
